578a8d6a25ee5af381f85e21ee55799c8425be39
Antonio <arestov.sec@gmail.com>
2020-04-20 23:11:57 +0700

    first commit

---
+++ str_format.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+age = 26
+name = 'Anton'
+
+print ('Возраст {0} -- {1} лет.'.format(name, age))
+print ('Почему {0} забавляется с этим Python?'.format(name, age))
+print ('{0:.7}'.format(1/3))
+print ('{0:*^8}'.format('hello'))
+print ('{book} написал {name}'.format(name='Anton', book='A this programm'))
