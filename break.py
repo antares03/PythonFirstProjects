467ac6c563019ccf8302226861e7d4f39a91c49b
antares <tofa_h_db9@mail.ru>
2020-04-22 18:07:15 +0700

    write for

---
+++ break.py
@@ -0,0 +1,6 @@
+while True:
+    s = input('input any where : ')
+    if s == 'exit':
+        break
+    print('long string : ', len(s))
+print('End')
--- for.py
+++ for.py
@@ -1,4 +1,4 @@
 for i in range(1, 5):
     print(i)
 else:
-    print('for loop is finished')
+    print('loop for finished')
