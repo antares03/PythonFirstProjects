39c2143664d38a64d97c5bacf1192530c79761ea
Bac <tofa_h_db9@mail.ru>
2020-04-24 00:36:59 +0700

    Home. First commit, add 9 files.

--- continue.py
+++ continue.py
@@ -1,2 +1,10 @@
 while True:
     s = input('input any where : ')
+    if s = 'Exit':
+        break
+    if len(s) < 3:
+        print('to small')
+        continue
+    print('Введённая строка достаточной длины')
+    # Разные другие действия здесь...
+
